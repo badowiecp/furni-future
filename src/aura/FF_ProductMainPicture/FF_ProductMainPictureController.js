0c8721d4fc22e959c9ff41f42d6add58e26cb3e1
Patryk Badowiec <patryk.badowiec@britenet.com.pl>
2021-06-10 18:36:48 +0200

    ProductMainPicture without console.logs

--- src/aura/FF_ProductMainPicture/FF_ProductMainPictureController.js
+++ src/aura/FF_ProductMainPicture/FF_ProductMainPictureController.js
@@ -1,23 +1,18 @@
 ({
 
     doInit : function(component,event,helper){
-        console.log('Enter main init');
        let action = component.get("c.getMainPicture");
 
        action.setParams({
            "productId": component.get("v.recordId")
        });
 
-       console.log('Record Id: ' + component.get("v.recordId"));
-
        action.setCallback(this, function(response) {
            let state = response.getState();
            if(state === 'SUCCESS') {
                component.set("v.contentVersionId", response.getReturnValue());
-               console.log('Picture Id: ' + component.get("v.contentVersionId"));
            }else{
                let error = response.getError();
-               console.log("Failed with state: " + state + ' Error: ' + error[0].message);
            }
        });
         $A.enqueueAction(action);
