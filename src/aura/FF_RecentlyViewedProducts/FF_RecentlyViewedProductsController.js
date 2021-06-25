b9f3aa56b5791fa6e3eea855ad249c5156c41eb5
Patryk Badowiec <patryk.badowiec@britenet.com.pl>
2021-06-25 22:12:28 +0200

    Controller clean up + labels

--- src/aura/FF_RecentlyViewedProducts/FF_RecentlyViewedProductsController.js
+++ src/aura/FF_RecentlyViewedProducts/FF_RecentlyViewedProductsController.js
@@ -4,63 +4,15 @@
     },
 
     goLeft: function (component, event, helper) {
-        let position = component.get("v.position");
-        let y = 0;
-        if (position > 94) {
-            y = 95;
-        } else {
-            y = position;
-        }
-        let x = 1;
-        let timeoutHandler = function (i) {
-            if (i < y) {
-                component.set("v.position", position - i);
-                i += 3;
-                setTimeout(() => {
-                        timeoutHandler(i);
-                    }
-                    , 15);
-            }
-        };
-        setTimeout(() => {
-                timeoutHandler(x);
-            }, 15
-        );
+        helper.scrollLeft(component,event);
     },
 
     goRight: function (component, event, helper) {
-        let position = component.get("v.position");
-        let y = 0;
-        if (position + 95 < component.get("v.maxPosition")) {
-            y = 95;
-        } else {
-            y = component.get("v.maxPosition") - position;
-        }
-        let x = 1;
-        let timeoutHandler = function (i) {
-            if (i < y) {
-                component.set("v.position", position + i);
-                i += 3;
-                setTimeout(() => {
-                        timeoutHandler(i);
-                    }
-                    , 15);
-            }
-        };
-        setTimeout(() => {
-                timeoutHandler(x);
-            }, 15
-        );
+        helper.scrollRight(component,event);
     },
 
     handleResize: function (component, event, helper) {
-        let max = component.find("wrapper").getElement().scrollWidth;
-        let shown = component.find("inner").getElement().offsetWidth;
-        if ((max - shown) === 0) {
-            component.set("v.maxPosition", 1);
-        } else {
-            component.set("v.maxPosition", shown - max);
-        }
+        helper.resize(component,event);
     }
 
 })
--- src/aura/FF_RecentlyViewedProducts/FF_RecentlyViewedProductsHelper.js
+++ src/aura/FF_RecentlyViewedProducts/FF_RecentlyViewedProductsHelper.js
@@ -18,6 +18,66 @@
             }
         });
         $A.enqueueAction(action);
+    },
+
+    scrollLeft : function(component,event){
+        let position = component.get("v.position");
+        let y = 0;
+        if (position > 94) {
+            y = 95;
+        } else {
+            y = position;
+        }
+        let x = 1;
+        let timeoutHandler = function (i) {
+            if (i < y) {
+                component.set("v.position", position - i);
+                i += 3;
+                setTimeout(() => {
+                        timeoutHandler(i);
+                    }
+                    , 15);
+            }
+        };
+        setTimeout(() => {
+                timeoutHandler(x);
+            }, 15
+        );
+    },
+
+    scrollRight : function(component,event){
+        let position = component.get("v.position");
+        let y = 0;
+        if (position + 95 < component.get("v.maxPosition")) {
+            y = 95;
+        } else {
+            y = component.get("v.maxPosition") - position;
+        }
+        let x = 1;
+        let timeoutHandler = function (i) {
+            if (i < y) {
+                component.set("v.position", position + i);
+                i += 3;
+                setTimeout(() => {
+                        timeoutHandler(i);
+                    }
+                    , 15);
+            }
+        };
+        setTimeout(() => {
+                timeoutHandler(x);
+            }, 15
+        );
+    },
+
+    resize : function(component,event){
+        let max = component.find("wrapper").getElement().scrollWidth;
+        let shown = component.find("inner").getElement().offsetWidth;
+        if ((max - shown) === 0) {
+            component.set("v.maxPosition", 1);
+        } else {
+            component.set("v.maxPosition", shown - max);
+        }
     }
 
 })
